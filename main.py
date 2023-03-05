b922a7b56af02ccae9672640d2830d7baf166fe7
ninadkale98 <ninadkale98@gmail.com>
2023-03-04 22:16:44 -0500

    Initial commit

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ port = 9559
 
 nao.InitProxy(ip)
 
-dance = threading.Thread( target= nao.dance )
-play_song = threading.Thread( target=nao.play_song )
+#dance = threading.Thread( target= nao.dance )
+#play_song = threading.Thread( target=nao.play_song )
 
-nao.say("I will start dance now")
+nao.say(" Hello Nao ")
 
-dance.start()
-play_song.start()
+# dance.start()
+# play_song.start()
 
-while True:
+# while True:
 
-    if play_song.is_alive() and not dance.is_alive():
-        dance = threading.Thread( target= nao.dance )
-        dance.start()
+#     if play_song.is_alive() and not dance.is_alive():
+#         dance = threading.Thread( target= nao.dance )
+#         dance.start()
 
-    if not play_song.is_alive() and not dance.is_alive():
-        break
+#     if not play_song.is_alive() and not dance.is_alive():
+#         break
 
-    time.sleep(1)
+#     time.sleep(1)
     
-nao.StopPlay()
+# nao.StopPlay()
