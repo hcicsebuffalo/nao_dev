0362c5072496f18ec1967f05f1c2bb57c023f567
ninadkale98 <ninadkale98@gmail.com>
2023-03-18 13:59:51 -0400

    tts file

--- main.py
+++ main.py
@@ -12,24 +12,52 @@ dance = threading.Thread( target= nao.dance )
 play_song = threading.Thread( target=nao.play_song )
 led = threading.Thread(target= nao.led_eye)
 
-nao.say(" Hello Manu ")
+nao.SetTTSVolume(0.9)
 
-dance.start()
-play_song.start()
-led.start()
+def temp():
+    nao.say(" Hello Everyone My name is Nao. I am humanoid robot. I work in Davis lab ")
 
-while True:
+say = threading.Thread(target=temp)
+hello = threading.Thread(target=nao.hello)
 
-    if play_song.is_alive() and not dance.is_alive():
-        dance = threading.Thread( target= nao.dance )
-        dance.start()
+say.start()
+hello.start()
 
-    if not play_song.is_alive() and not dance.is_alive():
-        break
+while say.is_alive() and hello.is_alive():
+    pass
+
+time.sleep(3)
+
+def temp2():
+    while 1:
+        detected, timestamp, facePosition = nao.DetectFace() 
+        if detected:
+            nao.say("Hello, How are you")
+        time.sleep(3)
+
+
+face = threading.Thread(target= temp2)
+
+face.start()
+#########################################
+
+# dance.start()
+# play_song.start()
+# led.start()
+
+
+# while True:
+
+#     if play_song.is_alive() and not dance.is_alive():
+#         dance = threading.Thread( target= nao.dance )
+#         dance.start()
+
+#     if not play_song.is_alive() and not dance.is_alive():
+#         break
     
-    time.sleep(1)
-    nao.led_eye()
+#     time.sleep(1)
+#     nao.led_eye()
 
-    #led.start()
+#     #led.start()
     
-# nao.StopPlay()
+# # nao.StopPlay()
