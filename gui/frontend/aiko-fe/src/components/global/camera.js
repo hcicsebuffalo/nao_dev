274507818131aa38cf98d073ffadc74d075d12da
ninadkale98 <ninadkale98@gmail.com>
2023-04-07 21:45:56 -0400

    layout chnages

--- gui/frontend/aiko-fe/src/components/global/camera.js
+++ gui/frontend/aiko-fe/src/components/global/camera.js
@@ -1,15 +1,45 @@
 import React, { useEffect, useState } from 'react'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
 import axios from 'axios'
 import logo from '../../logo.svg';
 import '../../App.css';
 
 const Camera = () => {
   return (
-    <div>
-      <img
-        src="http://127.0.0.1:8000/api/getfeed"
-        alt="Video"
-      />
+    <div style={{ padding: '1%' }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid xs={6} >
+            <div>
+              <h1>Video Feed</h1>
+              <img
+                src="http://127.0.0.1:8000/api/getfeed"
+                alt="Video"
+                style={{ height: '500px', width: '680px', padding: "2%"}}
+              />
+            </div>
+          </Grid>
+          <Grid xs={6}>
+            <div>
+              <h1>Chats</h1>
+            </div>
+          </Grid>
+          <Grid xs={6}>
+            <div>
+              <h1>Choose Actions</h1>
+            </div>
+          </Grid>
+          <Grid xs={6}>
+            <div>
+              <h1>Sensors</h1>
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
+      
     </div>
   );
 };
