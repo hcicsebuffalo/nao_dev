90132d37f0d9f50fd44bbaaeec38355285235011
ninadkale98 <sougato97@gmail.com>
2023-04-08 12:03:31 -0400

    final_change

--- python3/main.py
+++ python3/main.py
@@ -25,8 +25,7 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google_secret_key.json'
 # This GPT Conversation variable should be a global 
 conversation=[{"role":"system","content":"You are a helpful assistant"}]
 # Load the whisper model 
-model = whisper.load_model("medium.en")
-print("Whisper model import success")
+model = whisper.load_model("medium.en") #yamlrint("Whisper model import success")
 # # Audio clip name 
 audio_clip_path = os.getcwd() + "/recording.wav"
 
