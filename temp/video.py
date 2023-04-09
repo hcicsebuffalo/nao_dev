799a323da4b0037ddccfbb79dae50448f59a6cb8
ninadkale98 <sougato97@gmail.com>
2023-04-09 14:27:42 -0400

    video feed conn est

--- temp/video.py
+++ temp/video.py
@@ -7,6 +7,13 @@ import cv2
 import time
 from datetime import datetime
 import os
+import pika
+import base64
+import time 
+# Open a connection to RabbitMQ server
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+rabbit_channel = connection.channel()
+rabbit_channel.queue_declare(queue='image_queue')
 
 NAO_IP = "10.0.107.217"
 NAO_PORT = 9559
@@ -44,16 +51,24 @@ while True:
 
   img = (np.reshape(np.frombuffer(nao_image[6], dtype = '%iuint8' % nao_image[2]), (nao_image[1], nao_image[0], nao_image[2])))
   img = np.array(img)
+
   img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR) 
   print( (nao_image[1], nao_image[0], nao_image[2]))
   
-  cv2.imshow("Input", img)
+  #cv2.imshow("Input", img)
+
+  # Encode image to base64 string
+  retval, buffer = cv2.imencode('.jpg', img)
+  #image_str = base64.b64encode(buffer)#.decode('utf-8')
+  image_str = buffer.tobytes()
+  rabbit_channel.basic_publish(exchange='', routing_key='image_queue', body=image_str)
+  #print( len(image_str) , " :  " , i)
 
   k = cv2.waitKey(33)
   if k==27:    # Esc key to stop
     break
   
-
+connection.close()
 tts.releaseImage(subscriberID)
 tts.unsubscribe(subscriberID)
 cv2.destroyAllWindows()
