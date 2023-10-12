4bb14bb82dda0df3bef7e988108ee6a1ed1480bc
Ninad Kale <101186174+ninadkale98@users.noreply.github.com>
2023-10-12 19:25:58 -0400

    dataset

--- face_capture.py
+++ face_capture.py
@@ -5,26 +5,37 @@ cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
+    
+user = "praveen"
 
-image_captured = False
+# Define filenames for keys 1 to 6
+file_names = {
+    ord('1'): "dataset/" + user + "/face/front1.jpg",
+    ord('2'): "dataset/" + user + "/face/front2.jpg",
+    ord('3'): "dataset/" + user + "/face/occ1.jpg",
+    ord('4'): "dataset/" + user + "/face/occ2.jpg",
+    ord('5'): "dataset/" + user + "/face/pose1.jpg",
+    ord('6'): "dataset/" + user + "/face/pose2.jpg"
+}
 
-while not image_captured:
+while True:
     ret, frame = cap.read()
 
     if not ret:
         print("Error: Could not capture a frame.")
         exit()
 
-    cv2.imshow("Press 'a' to capture an image", frame)
+    cv2.imshow("frame", frame)
 
     key = cv2.waitKey(1) & 0xFF
 
-    if key == 97:  # 'a' key is pressed
-        cv2.imwrite("captured_image.jpg", frame)
+    # Check if key pressed is in the file_names dictionary
+    if key in file_names:
+        cv2.imwrite(file_names[key], frame)
+        print(f"Image captured and saved as '{file_names[key]}'")
 
-        image_captured = True
-        print("Image captured and saved as 'captured_image.jpg'")
+    elif key == 27:  # Esc key
+        break
 
 cap.release()
-
 cv2.destroyAllWindows()
